ed3d3616c806db5e1f84e189966e9b1518876397
21nkyu <adele7178@naver.com>
2021-08-10 23:44:03 +0900

    Update BOJ_19532.py
    ps

--- BOJ/BOJ_19532.py
+++ BOJ/BOJ_19532.py
@@ -1,14 +1,14 @@
 
 
 a, b, c, d, e, f = map(int, input().split())
-x = ((e*c)-(b*f))/((e*a)-(b*d))
-y = ((c*d)-(a*f))/((b*d)-(a*e))
-print(int(x),int(y))
+# x = ((e*c)-(b*f))/((e*a)-(b*d))
+# y = ((c*d)-(a*f))/((b*d)-(a*e))
+# print(int(x),int(y))
 
-#연립방정식을 그대로 옮김
+# #연립방정식을 그대로 옮김
 
 
-# for x in range(-999, 1000, 1):
-#     for y in range(-999, 1000, 1):
-#         ((e*a)-(b*d))*x == ((e*c)-(b*f))
-#         print(x)
+for x in range(-999, 1000, 1):
+    for y in range(-999, 1000, 1):
+        if a*x+b*y==c and d*x+e*y==f:
+            print(x, y)
