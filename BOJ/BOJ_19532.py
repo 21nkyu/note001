0e4d750d973a48a45bb8411895cce84c894f9e48
21nkyu <adele7178@naver.com>
2021-08-17 18:50:09 +0900

    Update BOJ_19532.py

--- BOJ/BOJ_19532.py
+++ BOJ/BOJ_19532.py
@@ -9,9 +9,9 @@ print(int(x),int(y))
 # ax + by = c *e     | aex + bey = ce
 # dx + ey = f *b    -| bdx + bey = bf
 #                      (ae-bd)x + (be-be)y = (ce - bf) 이런식으로 식을 만들어서 x로 정리하고 y로 정리해서 계산함
-#이게 내가 할 수 있는 선에는 최선이었음
 
+# a, b, c, d, e, f = map(int, input().split())
 # for x in range(-999, 1000, 1):
 #     for y in range(-999, 1000, 1):
-#         ((e*a)-(b*d))*x == ((e*c)-(b*f))
-#         print(x)
+#         if a*x + b*y == c and d*x + e*y == f:
+#             print(x, y)
