7736ad44e3fb3f4bdbc2dd60328d1f83e38a36ee
21nkyu <adele7178@naver.com>
2021-09-06 03:05:48 +0900

    20210905

---
+++ BOJ/working/BOJ_3613.py
@@ -0,0 +1,42 @@
+# https://www.acmicpc.net/problem/3613
+
+
+# # j = [h[i].lower() for i in range(len(h)) if h[i].isupper()]
+# j = []
+
+
+h = input()
+
+if '_' in h and h.islower() == True:
+    for i in range(len(h)-1):
+        if h[i] == '_':
+            print(h[i+1].upper(), end='')
+        else:
+            print(h[i], end='')
+
+elif '_' not in h and h.islower() ==False:
+    for i in range(len(h)):
+        if h[i].isupper():
+            print(h[i].lower(), end='')
+        else:
+            print(h[i], end='')
+
+else:
+    print("Error!")
+
+
+#=======================================
+# h = 'longAndShortButNo'
+# h=input()
+# for i in range(len(h)):
+#     if h[i].isupper():
+#         print(h[i].lower(), end='')
+#     else:
+#         print(h[i], end='')
+
+# h='long_and_home'
+# for i in range(len(h)):
+#     if h[i] == '_':
+#         print(h[i+1].upper(), end='')
+#     else:
+#         print(h[i], end='')
